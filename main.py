51ea13e1d2fb3a8e3d36d271f39fabc84456c947
Johnny Benitez <Johnny.Benitez@gmail.com>
2021-04-05 22:04:34 +0800

    updated main.py

--- main.py
+++ main.py
@@ -1,69 +1,4 @@
 
-
-# Artist = "Dave Brubeck"
-# Genre = "Jazz"
-# DurationInSeconds = 328
-# Year = "1994"
-
-# print(Artist)
-# print(Genre)
-# print(DurationInSeconds)
-# print(Year)
-
-
-# Artist = "Side A"
-# Genre = "Pop"
-# DurationInSeconds = 60
-# Year = "1994"
-
-# print(Artist)
-# print(Genre)
-# print(DurationInSeconds)
-# print(Year)
-
-# Artist = "Gary V"
-# Genre = "Christian"
-# DurationInSeconds = 100
-# Year = "1980"
-
-# print(Artist)
-# print(Genre)
-# print(DurationInSeconds)
-# print(Year)
-
-# Artist = "Martin Nivera"
-# Genre = "Ballad"
-# DurationInSeconds = 200
-# Year = "1990"
-
-# print(Artist)
-# print(Genre)
-# print(DurationInSeconds)
-# print(Year)
-
-# Exercise6
-
-# Blackshoes = {42: 2, 41: 3, 40: 4, 39: 1, 38: 0}
-# print (Blackshoes)
-# while (True):
-#     purchaseSize = int(input("Which shoe size do you want to buy?\n"))
-#     if purchaseSize < 0:
-#         break
-#     if Blackshoes[purchaseSize] > 0:
-#         Blackshoes[purchaseSize] -= 1
-#     else:
-#         print ("Shoes are no longer in stock")
-#         break
-#     print (Blackshoes)
-
-# favoriteSongs = {1: {'Title': 'Chances Are', 'Artist': 'Side A', 'Genre': 'Ballad', 'Duration': 328, 'Year': '1987'}, 2: {'Title': 'Salamat', 'Artist': 'The Dawn', 'Genre': 'Rock', 'Duration': 300, 'Year': '1980'}}
-
-# # for SongID in favoriteSongs:
-#     print (f'Song {SongID} is {favoriteSongs[SongID]} ')
-#     Song = favoriteSongs[SongID]
-#     for Attribute in Song:
-#         print (Attribute, Song[Attribute])
-
 def PrintFile(filename):
     # file = open(filename, 'r')
     # print (file.read())
@@ -101,7 +36,3 @@ print ('DONE')
 
 print('=' * 10)
 PrintFile('scoredb.csv')
-
-# OutputScoreDB = open('scoredb.csv', 'r')
-# print (OutputScoreDB.read())
-# OutputScoreDB.close()
